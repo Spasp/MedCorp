77b32071ddf3c5b0b852f255ba3250dfa90dc79f
Spyros Spanos <spaspyr@gmail.com>
2021-01-27 21:27:59 +0200

    Feature 5 complete

--- src/main/java/com/medicalcompany/springbootapp/demo/repository/SymptomRepository.java
+++ src/main/java/com/medicalcompany/springbootapp/demo/repository/SymptomRepository.java
@@ -1,10 +1,17 @@
 package com.medicalcompany.springbootapp.demo.repository;
 
+import com.medicalcompany.springbootapp.demo.domain.Patient;
 import com.medicalcompany.springbootapp.demo.domain.Symptom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SymptomRepository extends JpaRepository<Symptom, Long> {
+
+    @Query(value="SELECT  * FROM Symptom Where  Symptom.Patient_id=(:id)", nativeQuery = true)
+    List<Symptom> findSymptomById(Long id);
 
 }
